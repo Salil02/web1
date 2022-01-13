e1288313b6634c40335f268818ecd899b9330803
Salil02 <salilsingh1996@gmail.com>
2022-01-13 19:11:29 +0530

    Initial commit

---
+++ main/admin.py
@@ -0,0 +1,20 @@
+from django.contrib import admin
+from django.contrib.admin.options import ModelAdmin
+from .models import *
+
+# Register your models here.
+class CategoryAdmin( ModelAdmin):
+    list_display = ('name','id','icon')
+
+
+class ArticleAdmin(ModelAdmin):
+    list_display = ('title','category','user','date')
+    search_fields = ('category__name','tittle')
+    list_filter = ('date','user')
+
+
+
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Article, ArticleAdmin)
+
